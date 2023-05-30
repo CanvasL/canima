3ace8d55884ce41943db65cde36faff55f657189
CanvasL <746591811@qq.com>
2023-05-30 16:04:59 +0800

    feat: connect mysql

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -1,2 +1,27 @@
 package mysql
 
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"canvine/setting"
+)
+
+var db *sqlx.DB
+
+func Init(mysqlConfig *setting.MySQLConfig) (err error) {
+	connectUrl := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true&loc=Local", mysqlConfig.User, mysqlConfig.Password, mysqlConfig.Host, mysqlConfig.Port, mysqlConfig.DB)
+	if db, err = sqlx.Connect("mysql", connectUrl); err != nil {
+		fmt.Println("connect to MySql failed, err:", err)
+		return
+	}
+
+	db.SetMaxOpenConns(mysqlConfig.MaxOpenConns)
+	db.SetMaxIdleConns(mysqlConfig.MaxIdleConns)
+	return
+}
+
+func Close() {
+	_ = db.Close()
+}
+
--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"canvine/setting"
 	"canvine/router"
+	"canvine/dao/mysql"
 )
 
 func main() {
-	//1、加载配置
+	// 1. init setting
 	if err := setting.Init(); err != nil {
-		fmt.Println("init setting failed, ", err)
+		fmt.Println("init setting failed, err:", err)
 		return
 	}
+
+	// 2. init MySQL
+	if err := mysql.Init(setting.Config.MySQLConfig); err != nil {
+		fmt.Println("init mysql failed, err:", err)
+		return
+	}
+	defer mysql.Close()
 
 	r := router.SetupRouter()
 	if err := r.Run(fmt.Sprintf(":%d", setting.Config.Port)); err != nil {
